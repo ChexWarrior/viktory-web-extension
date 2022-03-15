c556de69d368e0010038089ab55b55e6f1629f2a
Andrew Lehman <aplehm@gmail.com>
2022-03-14 21:56:54 -0400

    Inject content script into window tab

--- popup/popup.js
+++ popup/popup.js
@@ -1,15 +1,20 @@
 const scrapeBtn = document.querySelector('#scrape');
 scrapeBtn.addEventListener('click', function(e) {
-  browser.windows.getAll()
+  browser.windows.getAll({ populate: true })
     .then(windows => {
-        document.querySelector('#windowCount').innerHTML = windows.length;
-        return browser.windows.getLastFocused();
-    })
-    .then(lastWindow => {
-      document.querySelector('#windowTitle').innerHTML = lastWindow.title;
-    })
-    .catch(error => {
-      document.querySelector('#windowTitle').innerHTML = error;
+      document.querySelector('#winLength').innerHTML = windows.length;
+      for (const w of windows) {
+        let title = w.title;
+        if (title.includes('Log')) {
+          //document.querySelector('div').innerHTML = title;
+          let windowTabId = w.tabs[0].id;
+          document.querySelector('div').innerHTML = windowTabId ?? 'hello';
+          browser.tabs.executeScript(
+            windowTabId,
+            { file: '/content_scripts/scrapeLog.js' }
+          );
+        }
+      }
     });
 });
 
