9e67bcf3cb66293f5d6a860692a5e9b1e58a7f41
Andrew Lehman <aplehm@gmail.com>
2022-03-20 16:59:13 -0400

    Toggle badgetext and button when opening popup

--- scripts/popup/popup.js
+++ scripts/popup/popup.js
@@ -1,3 +1,23 @@
+/**
+ * Check if any log windows are open:
+ * If any are then enable scrape button
+ * If there are none remove badgetext and disable button
+ */
+const scrapeBtn = document.querySelector('#scrape');
+const gameLogTitle = 'Game Log';
+browser.windows.getAll()
+  .then(windows => {
+    if (windows.some(w => w.title.includes(gameLogTitle))) {
+      scrapeBtn.removeAttribute('disabled');
+    } else {
+      scrapeBtn.setAttribute('disabled', 'true');
+      browser.browserAction.setBadgeText({
+        text: ''
+      });
+    }
+  })
+
+
 // const scrapeBtn = document.querySelector('#scrape');
 // scrapeBtn.addEventListener('click', function(e) {
 //   browser.windows.getAll({ populate: true })
