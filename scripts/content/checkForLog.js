519b4035f54a7f3ed43faf6bf8754bf86e9015d7
Andrew Lehman <aplehm@gmail.com>
2022-03-20 20:14:00 -0400

    Grab game id

--- scripts/content/checkForLog.js
+++ scripts/content/checkForLog.js
@@ -2,18 +2,19 @@ console.log('checkForLog loaded...');
 let totalTime = 0;
 const interval = 1000;
 const maxTime = 5000;
+const gameId = window.opener.window.location.search.substr(1);
 
 // Check for log and add indicator to browser_action if found
 let intervalId = setInterval(() => {
   let logContainer = document.querySelector('#Foundation_Elemental_1_log');
   if (logContainer) {
-    console.log('Log found...');
+    console.log(`Log found for game ${gameId}`);
     browser.runtime.sendMessage({
       logFound: true,
     });
     clearInterval(intervalId);
   } else {
-    console.log('Log not found...');
+    console.log(`Log not found for game ${gameId}`);
   }
 
   if (totalTime >= maxTime) {
