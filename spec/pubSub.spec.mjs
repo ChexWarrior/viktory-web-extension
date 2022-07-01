92cdbdd09dc6557d60ac4ce155b1320296576abc
Andrew Lehman <aplehm@gmail.com>
2022-07-01 11:19:27 -0400

    Add another test

--- spec/pubSub.spec.mjs
+++ spec/pubSub.spec.mjs
@@ -5,7 +5,15 @@ describe('Testing PubSub class', function() {
   it('Subscribing to a new topic returns index 0', function() {
     const pubSub = new PubSub();
     let index = pubSub.subscribe('event', () => 1);
-    
+
     expect(index).toBe(0);
+  });
+
+  it('Subscribing to the same topic twice returns index 1', function() {
+    const pubSub = new PubSub();
+    pubSub.subscribe('event', () => 1);
+    let index = pubSub.subscribe('event', () => 2);
+
+    expect(index).toBe(1);
   });
 });
