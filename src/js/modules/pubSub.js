a6e4341bc0496a98245e7359f5dea5562990b3a9
Andrew Lehman <aplehm@gmail.com>
2022-07-01 10:24:39 -0400

    Add event pubsub module

---
+++ src/js/modules/pubSub.js
@@ -0,0 +1,40 @@
+class PubSub {
+
+  constructor() {
+    this.topics = new Map();
+  }
+
+  subscribe(topic, listener) {
+    // Create topic if we don't already have it
+    if (!this.topics.has(topic)) {
+      this.topics.set(topic, []);
+    }
+
+    const currentListeners = this.topics.get(topic);
+    const index = currentListeners.push(listener) - 1;
+    this.topics.set(topic, currentListeners);
+
+    // Return index so calling code can remove topic
+    return index;
+  }
+
+  remove(topic, index) {
+    if (index < 0 || !this.topics.has(topic)) {
+      return false;
+    }
+
+    let currentListeners = this.topics.get(topic);
+
+    if (index >= currentListeners.length) {
+      return false;
+    }
+
+    currentListeners = currentListeners.splice(index, 1);
+
+    return true;
+  }
+
+  publish(topic, info) {
+
+  }
+}
