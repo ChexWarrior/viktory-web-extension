ade8b80def97230f54fa9440d9e8b35eceba60a3
Andrew Lehman <aplehm@gmail.com>
2022-07-01 13:34:21 -0400

    Add tests for removal methods

--- spec/pubSub.spec.mjs
+++ spec/pubSub.spec.mjs
@@ -23,7 +23,31 @@ describe('Testing PubSub class', function() {
     pubSub.subscribe('event', () => count += 1);
     pubSub.subscribe('event', () => count += 2);
     pubSub.publish('event');
-    
+
     expect(count).toBe(3);
+  });
+
+  it('Correctly removes listener for topic by index', function() {
+    const pubSub = new PubSub();
+    let count = 0;
+    let index = pubSub.subscribe('event', () => count += 2);
+    pubSub.subscribe('event', () => count += 1);
+    pubSub.removeListener('event', index);
+    pubSub.publish('event');
+
+    expect(count).toBe(1);
+  });
+
+  it('Correctly removes all listeners for topic', function() {
+    const pubSub = new PubSub();
+    let count = 0;
+    pubSub.subscribe('event', () => count += 1);
+    pubSub.subscribe('event', () => count += 1);
+    pubSub.subscribe('event', () => count += 1);
+    pubSub.subscribe('event', () => count += 1);
+    pubSub.removeTopic('event');
+    pubSub.publish('event');
+
+    expect(count).toBe(0);
   });
 });
