7b15d7deceee8e45f0e7c0077c939e5050f59882
Andrew Lehman <aplehm@gmail.com>
2022-07-21 19:54:29 -0400

    Open new game page when submitting form

--- src/js/modules/gameStart.mjs
+++ src/js/modules/gameStart.mjs
@@ -8,30 +8,22 @@ class GameStart extends TabTarget {
    */
   constructor(targetForm, pubSub) {
     super(targetForm, pubSub);
+    this.newGameUrl = 'http://gamesbyemail.com/Games/Viktory2';
     this.form = targetForm;
-    this.createGameBtn = this.form.querySelector('button');
-    this.createGameBtn.addEventListener('click', (event) => {
+    this.form.addEventListener('submit', (event) => {
       event.preventDefault();
       this.createGame(event);
     });
   }
 
   createGame(event) {
-    // Check if we're on the new game page otherwise navigate us there...
-    browser.tabs.executeScript({ file: '/src/js/content/startNewGame.js' })
-      .then(response => {
-        return browser.windows.getCurrent({
-          populate: true
-        });
-      })
-      .then(currentWindow => {
-        const [activeTab] = currentWindow.tabs.filter(t => t.active);
-
-        return browser.tabs.sendMessage(
-          activeTab.id,
-          { greeting: 'This is a message!' },
-        );
-      });
+    browser.tabs.create({
+      active: true,
+      url: this.newGameUrl,
+    })
+    .then(tab => {
+      console.log('tab created!');
+    });
   }
 }
 
