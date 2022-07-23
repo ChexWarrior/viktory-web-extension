1747d9cb7cfb2ad98155be4cc85df3a0c383a31d
Andrew Lehman <aplehm@gmail.com>
2022-07-23 09:41:37 -0400

    Click proper number of players on form

--- src/js/content/startGame.js
+++ src/js/content/startGame.js
@@ -1,9 +1,16 @@
 console.log('start new game script called...');
 
-const gameTitleInput = document.querySelector('#Foundation_Elemental_5_GameTitle');
 
 // Setup listener from gameStart component
 browser.runtime.onMessage.addListener((gameInfo, sender) => {
-  const { title } = gameInfo;
+  const { title, numPlayers, playerInfo } = gameInfo;
+
+  // Grab page form elements
+  const gameTitleInput = document.querySelector('#Foundation_Elemental_5_GameTitle');
+  const numPlayerInput = document.querySelector(`input[type="radio"][value="${numPlayers}"]`);
+
+  // Set game values
   gameTitleInput.value = title;
+  numPlayerInput.click();
+  
 });
