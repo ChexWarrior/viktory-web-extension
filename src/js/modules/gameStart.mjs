d6519c1fef7a002a642dad809d34204e5c09f6d1
Andrew Lehman <aplehm@gmail.com>
2022-07-22 20:33:20 -0400

    Show hide player info fields depending on num players selected

--- src/js/modules/gameStart.mjs
+++ src/js/modules/gameStart.mjs
@@ -8,12 +8,50 @@ class GameStart extends TabTarget {
    */
   constructor(targetForm, pubSub) {
     super(targetForm, pubSub);
+    this.lastTabId = null;
     this.newGameUrl = 'http://gamesbyemail.com/Games/Viktory2';
     this.form = targetForm;
-    this.lastTabId = null;
+    this.numPlayerContainer = this.form.querySelector('div.numPlayers');
+    this.playerInfoFields = this.form.querySelectorAll('div[data-player-info]');
+
+    // Setup event handlers
+    pubSub.subscribe('numPlayersChanged', this.updateNumPlayers, this);
+
+    // Submit form
     this.form.addEventListener('submit', (event) => {
       event.preventDefault();
       this.createGame(event);
+    });
+
+    // Change number of players
+    this.numPlayerContainer.addEventListener('click', (event) => {
+      const { target } = event;
+
+      if (target.tagName === 'INPUT') {
+        const numPlayers = parseInt(target.value, 10);
+        pubSub.publish('numPlayersChanged', numPlayers);
+      }
+    });
+
+    // Handle the initial number of players
+    let initialNumPlayers = this.numPlayerContainer.querySelector('input[checked]').value;
+    pubSub.publish('numPlayersChanged', initialNumPlayers);
+  }
+
+  updateNumPlayers(numPlayers) {
+    /**
+    * Show playerInfo fields with a number <= numPlayers and
+    * hide those with a number > numPlayers
+    */
+    this.playerInfoFields.forEach(field => {
+      const playerNum = parseInt(field.dataset.playerInfo, 10);
+
+      if (playerNum <= numPlayers) {
+        field.classList.remove('is-hidden');
+        return;
+      }
+
+      field.classList.add('is-hidden');
     });
   }
 
